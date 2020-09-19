0956fd8ad9afe5c1b38ccf3655194082a8664fce
pyatnicyn.stanislav <pyatnicyn.stanislav@yandex.ru>
2020-09-19 18:29:51 +0300

    Added RecyclerList and adapter in TaskActivity

--- app/src/main/java/com/spappstudio/conspectmanager/TasksActivity.java
+++ app/src/main/java/com/spappstudio/conspectmanager/TasksActivity.java
@@ -4,13 +4,26 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.spappstudio.conspectmanager.adapters.TasksRecyclerAdapter;
+import com.spappstudio.conspectmanager.objects.Task;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class TasksActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
+
+    private ArrayList<Task> tasks;
+    private DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +40,19 @@ public class TasksActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        dbHelper = new DBHelper(this);
+        //tasks = dbHelper.getAllTasks();
+        tasks = new ArrayList<>();
+        tasks.add(new Task(-1, "Task 1", "Math", "19.09.2020", "24.09.2020", "To do somthing\nSecond string\nAnd another string\nIt's the task", 0, 0, 0));
+        tasks.add(new Task(-1, "Task 2", "Physics", "19.09.2020", "24.09.2020", "To do somthing\nSecond string\nAnd another string\nIt's the task", 0, 0, 0));
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+
+        recyclerAdapter = new TasksRecyclerAdapter(tasks);
+        recyclerView.setAdapter(recyclerAdapter);
     }
 }
