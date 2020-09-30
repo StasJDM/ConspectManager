f2d9593b571fb8f406290d195631e24295f43865
pyatnicyn.stanislav <pyatnicyn.stanislav@yandex.ru>
2020-09-30 22:57:52 +0300

    Added OneTaskActivity, OnClick in TasksRecyclerAdapter

---
+++ app/src/main/java/com/spappstudio/conspectmanager/OneTaskActivity.java
@@ -0,0 +1,14 @@
+package com.spappstudio.conspectmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OneTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_task);
+    }
+}
